ec0fe5f4d39fb5894af2e8299adfa3ec3f3bd266
Hein Htet Zaw <HHZ@NiMg-Macbook-Pro.local>
2021-05-30 00:50:09 +0630

    first

---
+++ JS/news.js
@@ -0,0 +1,185 @@
+let paper = {
+    "status": "ok",
+    "totalResults": 19055,
+    "articles": [{
+        "source": {"id": "newsweek", "name": "Newsweek"},
+        "author": "Seren Morris",
+        "title": "Tesla Slides Down Snowy Street and Collides With Car in Viral Video",
+        "description": "After the Tesla crashed into a parked car, a second video showed another car losing control on the same hilly street in Glasgow, Scotland.",
+        "url": "https://www.newsweek.com/tesla-slides-down-snowy-street-collides-car-viral-video-1567933",
+        "urlToImage": "https://d.newsweek.com/en/full/1729713/glasgow-snow.jpg",
+        "publishedAt": "2021-02-09T16:48:33Z",
+        "content": "A video of a Tesla sliding down a steep, snowy street in Scotland and crashing into another car has gone viral on Twitter, as the U.K. feels the effects of Storm Darcy.\r\nDes Tinney shared the video o… [+2541 chars]"
+    }, {
+        "source": {"id": "independent", "name": "Independent"},
+        "author": "Anthony Cuthbertson",
+        "title": "‘Tesla insider’ leaked huge bitcoin investment on Reddit over a month ago but was laughed at",
+        "description": "'I told you so,' anonymous Redditor posted after the company announced $1.5bn investment",
+        "url": "https://www.independent.co.uk/life-style/gadgets-and-tech/tesla-bitcoin-price-reddit-leak-b1799837.html",
+        "urlToImage": "https://static.independent.co.uk/2021/02/09/16/tesla%20bitcoin%20price%20leak.jpg?width=1200&auto=webp&quality=75",
+        "publishedAt": "2021-02-09T16:48:10Z",
+        "content": "A Reddit user claiming to be a Tesla software engineer posted about the company's massive bitcoin investment last month but was laughed at and largely ignored.\r\nThe post was shared on bitcoin and Tes… [+1970 chars]"
+    }, {
+        "source": {"id": null, "name": "ValueWalk"},
+        "author": "David Pinsen",
+        "title": "Silver: The Family Circus Of Investments",
+        "description": "Moving On From Precious Metals In our last post (Precious Metals: Time To Move On), we explained why our system had moved on from precious metals names. In a nutshell, their recent returns had been too weak, causing them to fail one of our preliminary screens…",
+        "url": "https://www.valuewalk.com/mstr-riot-family-circus-investments/",
+        "urlToImage": "https://www.valuewalk.com/wp-content/uploads/2021/02/115254.jpg",
+        "publishedAt": "2021-02-09T16:45:24Z",
+        "content": "Moving On From Precious Metals\r\nIn our last post (Precious Metals: Time To Move On), we explained why our system had moved on from precious metals names. In a nutshell, their recent returns had been … [+3948 chars]"
+    }, {
+        "source": {"id": null, "name": "Www.hln.be"},
+        "author": "HR",
+        "title": "Bitcoin-stunt van Tesla lijkt maand geleden aangekondigd door mysterieuze Reddit-gebruiker",
+        "description": "Het lijkt erop dat een Reddit-gebruiker meer dan een maand geleden de gigantische bitcoin-aankoop van autofabrikant Tesla bekendgemaakt heeft. Het nieuws over die aankoop stuurde de koers van de cryptomunt naar een nieuw record. De auteur van de post op Reddi…",
+        "url": "https://www.hln.be/geld/bitcoin-stunt-van-tesla-lijkt-maand-geleden-aangekondigd-door-mysterieuze-reddit-gebruiker~a680c21b/",
+        "urlToImage": "https://images0.persgroep.net/rcs/q2jEkEh2CMXktex42K19-appQR0/diocontent/182106877/_fill/1200/630/?appId=21791a8992982cd8da851550a453bd7f&quality=0.7",
+        "publishedAt": "2021-02-09T16:45:13Z",
+        "content": "Het bericht werd blijkbaar niet opgemerkt toen het verscheen, maar kwam dinsdag, één dag nadat de aankoop van Tesla algemeen bekendraakte, onder de aandacht. Ik ben een softwareontwikkelaar die werkt… [+1986 chars]"
+    }, {
+        "source": {"id": null, "name": "ValueWalk"},
+        "author": "Michelle Jones",
+        "title": "Bitcoin rally triggered by Tesla slows, but MicroStrategy still rising",
+        "description": "Tesla drove another bitcoin rally by purchasing some of the cryptocurrency. The bitcoin rally has slowed now since Tesla’s purchase, although the price remains above $40,000. Tesla CEO Elon Musk appears to have gotten the idea to buy bitcoin from MicroStrateg…",
+        "url": "https://www.valuewalk.com/tesla-bitcoin-rally-slows-microstrategy-rising/",
+        "urlToImage": "https://www.valuewalk.com/wp-content/uploads/2020/11/bitcoin_1604495816.jpg",
+        "publishedAt": "2021-02-09T16:45:02Z",
+        "content": "Tesla drove another bitcoin rally by purchasing some of the cryptocurrency. The bitcoin rally has slowed now since Tesla’s purchase, although the price remains above $40,000. Tesla CEO Elon Musk appe… [+2949 chars]"
+    }, {
+        "source": {"id": null, "name": "manager-magazin.de"},
+        "author": "Christoph Rottwilm",
+        "title": "Tesla, Elon Musk, Siemens Energy, Bumble - das war Dienstag, 9.2.2021",
+        "description": "Jeden Abend fassen wir die wichtigsten Wirtschaftsnews des Tages zusammen. Heute mit einer App-Gründerin, die an die Börse strebt, einer Gründerfamilie, die sich zurückzieht, und einer ganzen Gründergeneration, die Sinn über Profit stellt - angeblich.",
+        "url": "https://www.manager-magazin.de/ueber-uns/tesla-elon-musk-siemens-energy-bumble-das-war-dienstag-9-2-2021-a-f9ee4cd3-a7c8-4167-a600-9a91f62ee758",
+        "urlToImage": "https://cdn.prod.www.manager-magazin.de/images/b50e8c6a-4838-488d-b1f7-8bfb60db8f6b_w1195_r1.77_fpx66.34_fpy49.93.png",
+        "publishedAt": "2021-02-09T16:43:35Z",
+        "content": "Auch 24 Stunden nach Bekanntwerden scheint der Einstieg von Tesla in den Bitcoin der wichtigsten Kryptowährung noch Auftrieb zu verleihen. Der Preis des Bitcoins stieg am Dienstag weiter Richtung 50.… [+1512 chars]"
+    }, {
+        "source": {"id": null, "name": "CNA"},
+        "author": "CNA",
+        "title": "Musk's Bitcoin investment follows months of Twitter talk",
+        "description": "Tesla's US$1.5 billion Bitcoin investment unveiled on Monday may not surprise CEO Elon Musk's followers on Twitter, where he has shown himself as a major proponent of the soaring cryptocurrency.",
+        "url": "https://www.channelnewsasia.com/news/business/musk-s-bitcoin-investment-follows-months-of-twitter-talk-14149836",
+        "urlToImage": "https://cna-sg-res.cloudinary.com/image/upload/q_auto,f_auto/image/14149834/16x9/991/557/f681da497420bf585e77eef82653af5/ka/file-photo--spacex-founder-and-chief-engineer-elon-musk-looks-at-his-mobile-phone-during-a-post-launch-news-conference-to-discuss-the--spacex-crew-dragon-astronaut-capsule-in-flight-abort-test-at-the--3.jpg",
+        "publishedAt": "2021-02-09T16:40:30Z",
+        "content": "REUTERS: Tesla's US$1.5 billion Bitcoin investment unveiled on Monday may not surprise CEO Elon Musk's followers on Twitter, where he has shown himself as a major proponent of the soaring cryptocurre… [+1928 chars]"
+    }, {
+        "source": {"id": "fox-news", "name": "Fox News"},
+        "author": "Danielle Wallace",
+        "title": "California DA responds to attacks against elderly Asians, as activists claim coronavirus pandemic to blame",
+        "description": "A northern California district attorney announced Monday that a new special response unit was created in the wake of recent attacks against Asians, particularly elderly Asians, in Oakland’s Chinatown, as activists have argued that other violence in San Franci…",
+        "url": "https://www.foxnews.com/us/california-oakland-asians-elderly-attacks-coronavirus-pandemic-rhetoric-lunar-new-year",
+        "urlToImage": "https://cf-images.us-east-1.prod.boltdns.net/v1/static/694940094001/914d1cbe-e4c6-40ba-b351-d25efe317f2d/526b466c-a655-4c1b-936a-5be9663b1467/1280x720/match/image.jpg",
+        "publishedAt": "2021-02-09T16:39:48Z",
+        "content": "A northern California district attorney announced Monday that a new special response unit was created in the wake of recent attacks against Asians, particularly elderly Asians, in Oaklands Chinatown,… [+6114 chars]"
+    }, {
+        "source": {"id": "business-insider", "name": "Business Insider"},
+        "author": "hrobertson@businessinsider.com (Harry Robertson)",
+        "title": "Elon Musk's $1.5 billion bet on bitcoin has exposed Tesla to 'immense' risk that could wipe out its profits, analysts say",
+        "description": "Elon Musk's Tesla will have to revalue its new $1.5 billion bitcoin holding regularly, meaning a plunge in the price could batter the firm's profits.",
+        "url": "https://www.businessinsider.com/elon-musk-tesla-bitcoin-bet-immense-risk-price-fall-2021-2",
+        "urlToImage": "https://i.insider.com/6022b767cca90200129b7380?width=1200&format=jpeg",
+        "publishedAt": "2021-02-09T16:37:32Z",
+        "content": "Tesla's decision to buy up $1.5 billion of bitcoin exposes Elon Musk's electric car company to \"immense\" risks that could wipe out its annual profit if the cryptocurrency's price plunges, a Saxo Bank… [+3102 chars]"
+    }, {
+        "source": {"id": null, "name": "Electrek"},
+        "author": "Fred Lambert",
+        "title": "Tesla will complete R&D center this year to design new $25,000 electric car",
+        "description": "Tesla is on track this year to complete its new R&D center in Shanghai that will engineer and design its new $25,000 electric car. Tesla R&D center in China Just over a year ago, Tesla announced plans to establish a new R&D and design center in China to build…",
+        "url": "https://electrek.co/2021/02/09/tesla-complete-rd-center-make-25000-new-electric-car/",
+        "urlToImage": "https://i1.wp.com/electrek.co/wp-content/uploads/sites/3/2020/07/Tesla-hatchback.jpg?resize=1200%2C628&quality=82&strip=all&ssl=1",
+        "publishedAt": "2021-02-09T16:35:03Z",
+        "content": "Tesla is on track this year to complete its new R&amp;D center in Shanghai that will engineer and design its new $25,000 electric car.\r\nTesla R&amp;D center in China\r\nJust over a year ago, Tesla anno… [+2731 chars]"
+    }, {
+        "source": {"id": null, "name": "CleanTechnica"},
+        "author": "Zachary Shahan",
+        "title": "Brooklyn Getting USA’s 1st EV Fast Charging “Superhub”",
+        "description": "Tesla builds Supercharger stations across the world with dozens of supercharging stalls at many new stations. However, EV charging stations from other companies often just have a few. In the US, Electrify America is now typically putting in stations with seve…",
+        "url": "https://cleantechnica.com/2021/02/09/brooklyn-getting-usas-1st-ev-fast-charging-superhub/",
+        "urlToImage": "https://cleantechnica.com/files/2021/02/Revel-EV-Charging-Superhub-Brooklyn-NYC-3.png",
+        "publishedAt": "2021-02-09T16:32:32Z",
+        "content": "Tesla builds Supercharger stations across the world with dozens of supercharging stalls at many new stations. However, EV charging stations from other companies often just have a few. In the US, Elec… [+4560 chars]"
+    }, {
+        "source": {"id": null, "name": "Wykop.pl"},
+        "author": "darosoldier",
+        "title": "Tesla inwestuje w bitcoina. Kurs kryptowaluty wzrósł do 48 tys. dolarów",
+        "description": "Tesla zainwestowała ok. 1,5 mld dolarów w bitcoina. Koncern Elona Muska może też wkrótce zaakceptować cyfrową walutę również jako jedną z opcji płatności za swoje samochody. To nie wszystko.",
+        "url": "https://www.wykop.pl/link/5948453/tesla-inwestuje-w-bitcoina-kurs-kryptowaluty-wzrosl-do-48-tys-dolarow/",
+        "urlToImage": "https://www.wykop.pl/cdn/c3397993/link_16128881773b1MncHJkXyMGFaf9w0v9b,w1200h627f.jpg",
+        "publishedAt": "2021-02-09T16:31:48Z",
+        "content": "Drogi Wykopowiczu\r\nw zaczonym linku do Polityki Prywatnoci przypominamy podstawowe informacje z zakresu przetwarzania danych osobowych dostarczanych przez Ciebie podczas korzystania z naszego serwisu… [+108 chars]"
+    }, {
+        "source": {"id": null, "name": "Demorgen.be"},
+        "author": "Freek Evers",
+        "title": "Hip, exclusief en verboden in China: is de app Clubhouse de hype echt waard?",
+        "description": "De app Clubhouse in het kort: gecensureerd door China, vrouwonvriendelijk, gehypet door techondernemer Elon Musk en enkel toegankelijk na een uitnodiging. De ideale cocktail voor media-aandacht. Maar brengt het ook iets nieuws?",
+        "url": "https://www.demorgen.be/tech-wetenschap/hip-exclusief-en-verboden-in-china-is-de-app-clubhouse-de-hype-echt-waard~beeb06ff/",
+        "urlToImage": "https://images0.persgroep.net/rcs/MqZE6LFqhYIpw3UY4szf0v3UAtM/diocontent/182092447/_fitwidth/763?appId=93a17a8fd81db0de025c8abd1cca1279&quality=0.8",
+        "publishedAt": "2021-02-09T16:30:46Z",
+        "content": "De lockdown duurt ondertussen zo lang dat een mens zou verlangen naar recepties met slechte hapjes, lauwe drankjes en betekenisloze smalltalk. Wie dat nog eens wil ervaren, moet de socialmedia-app Cl… [+3650 chars]"
+    }, {
+        "source": {"id": null, "name": "Genbeta.com"},
+        "author": "Toni Castillo",
+        "title": "Por qué el Banco de España y la CNMV han vuelto a advertir sobre el \"riesgo\" de invertir en criptomonedas",
+        "description": "La Comisión Nacional del Mercado de Valores y el Banco de España se han unido este martes para publicar un comunicado conjunto en el que alertan del \"riesgo\" de las criptomonedas como inversión. Una advertencia que actualiza la que ya hicieron en 2018, asegur…",
+        "url": "https://www.genbeta.com/a-fondo/que-banco-espana-cnmv-han-vuelto-a-advertir-riesgo-invertir-criptomonedas",
+        "urlToImage": "https://i.blogs.es/9f56b2/cryptocurrency-3409725_1920/840_560.jpg",
+        "publishedAt": "2021-02-09T16:30:34Z",
+        "content": "La Comisión Nacional del Mercado de Valores y el Banco de España se han unido este martes para publicar un comunicado conjunto en el que alertan del \"riesgo\" de las criptomonedas como inversión. Una … [+5209 chars]"
+    }, {
+        "source": {"id": null, "name": "Abertoatedemadrugada.com"},
+        "author": "Carlos Martins",
+        "title": "Notícias do dia",
+        "description": "Primeiras imagens do Android 12; CD Projekt atacado por ransomware; Huawei quer \"reboot\" na relação com os EUA; Actualização de Teslas dá show de luz",
+        "url": "https://abertoatedemadrugada.com/2021/02/noticias-do-dia_9.html",
+        "urlToImage": "https://1.bp.blogspot.com/-gJGoHPkkvOQ/YCKf__d2ltI/AAAAAAAGGiw/ELPvKu-F3e0-RLMydAUHLSHWNqV_mFvQwCLcBGAsYHQ/w1200-h630-p-k-no-nu/android12.jpg",
+        "publishedAt": "2021-02-09T16:30:00Z",
+        "content": "App Barcode Scanner leva malware a 10 milhões de Androids com uma actualização após mudança de dono; Xiaomi revela Mi TV Q1 de 75\"; Telegram, TikTok e Signal lideram downloads de apps em Janeiro; dam… [+4728 chars]"
+    }, {
+        "source": {"id": null, "name": "Iphoneitalia.com"},
+        "author": "Giuseppe Migliorino",
+        "title": "Apple Car tra BMW, Magna Steyr e Regno Unito",
+        "description": "Una tra BMW e Magna Steyr potrebbe davvero produrre la prossima Apple Car?\nLink all'articolo originale: Apple Car tra BMW, Magna Steyr e Regno Unito",
+        "url": "https://www.iphoneitalia.com/756705/apple-car-bmw-magna",
+        "urlToImage": "https://static.iphoneitalia.com/wp-content/uploads/2021/01/apple-car-mercedes.jpg",
+        "publishedAt": "2021-02-09T16:22:44Z",
+        "content": "Le ultime voci sulla Apple Car parlano di possibili collaborazioni con BMW o Magna, ma anche di un forte interessamento del governo britannico verso la produzione della prima vettura elettrica di App… [+4082 chars]"
+    }, {
+        "source": {"id": null, "name": "Süddeutsche Zeitung"},
+        "author": "Von Victor Gojdka, Frankfurt, und Claus Hulverscheidt",
+        "title": "Tesla: Was will Musk mit all den Bitcoins?",
+        "description": "Tesla hat 1,5 Milliarden Dollar in Bitcoins investiert. Das treibt den Kurs der Kryptowährung auf Höchststände - und ist riskant für den Tesla-Chef.",
+        "url": "https://www.sueddeutsche.de/wirtschaft/elon-musk-tesla-bitcoin-1.5201172",
+        "urlToImage": "https://www.sueddeutsche.de/image/sz.1.5201173/1200x675?v=1612887575",
+        "publishedAt": "2021-02-09T16:22:17Z",
+        "content": "Tesla hat 1,5 Milliarden Dollar in Bitcoins investiert. Das treibt den Kurs der Kryptowährung auf Höchststände - und ist riskant für den Tesla-Chef.Um Elektroautos an die Frau oder den Mann zu bringe… [+5034 chars]"
+    }, {
+        "source": {"id": null, "name": "Gazeta.ru"},
+        "author": "Ирина Власова",
+        "title": "Эксперты оценили перспективы курса биткоина после вложений Илона Маска",
+        "description": "Илон Маск снова оживил криптовалютный рынок. После массовой покупки биткоинов компанией Tesla, цена на токены пошла вверх. Эксперты считают, что с наибольшей вероятностью прецедент увеличит привлекательность и стоимость криптовалют, однако их волатильность та…",
+        "url": "https://www.gazeta.ru/business/2021/02/09/13472918.shtml",
+        "urlToImage": "https://img.gazeta.ru/files3/948/13472948/2021-02-09T145719Z_405819235_RC22PL9BHOP6_RTRMADP_3_CRYPTO-CURRENCY-pic905-895x505-57803.jpg",
+        "publishedAt": "2021-02-09T16:19:54Z",
+        "content": ". , 9 $48 . Tesla: $1,5 .\r\n (SEC) 10%.  40%- .\r\n« 2021 ,   , », —  Tesla.  , , , . Tesla .\r\n«, , , , , , », \r\n— «» . , :\r\n« , , . , 50-70% », — .\r\n — , $1,5 — (8% ), Exante . , Tesla, , .\r\n« MicroStr… [+359 chars]"
+    }, {
+        "source": {"id": null, "name": "Sina.com.cn"},
+        "author": "□周武英",
+        "title": "从特斯拉大笔投资比特币说起 - 新浪网",
+        "description": "<ol><li>从特斯拉大笔投资比特币说起  新浪网\r\n</li><li>比特币首次站上4.7万美元一枚比特币能换一斤半黄金- 数字货币/ 区块链  cnBeta\r\n</li><li>比特币市值超脸书：特斯拉力挺今年价格或上10万美元  驱动之家\r\n</li><li>特斯拉被五部门联合约谈，还宣布15亿美元狂买比特币，币圈全炸了  新浪网\r\n</li><li>外媒：特斯拉的比特币投资或对其气候声誉和底线造成不利影响- Tesla 特斯拉电动汽车  cnBeta\r\n</li><li>在Google 新闻上查看完整报…",
+        "url": "https://finance.sina.com.cn/tech/2021-02-10/doc-ikftpnny6069516.shtml",
+        "urlToImage": "",
+        "publishedAt": "2021-02-09T16:19:30Z",
+        "content": "28SEC10-Q158000\r\n 15\r\n 1202020207.21200320198.62202015.832020\r\n 20207.2112.722021 15.8166%4.012020152020\r\n 1020020202014.9\r\n 202028491941920202020291223.37505015\r\n 158000202040050%500-60020\r\n 1525220… [+41 chars]"
+    }, {
+        "source": {"id": null, "name": "MarketWatch"},
+        "author": "Steve Goldstein",
+        "title": ": Electricity to power bitcoin surges to new heights as price gets Tesla boost",
+        "description": "Tesla's disclosure that it has purchased $1.5 billion of bitcoin has had the effect of sending the estimated electricity consumption of the cryptocurrency to new levels.",
+        "url": "https://www.marketwatch.com/story/electricity-to-power-bitcoin-surges-to-new-heights-as-price-gets-tesla-boost-11612887550",
+        "urlToImage": "https://images.mktw.net/im-287414/social",
+        "publishedAt": "2021-02-09T16:19:00Z",
+        "content": "Teslas disclosure that it has purchased $1.5 billion of bitcoin has had the effect of sending the estimated electricity consumption of the cryptocurrency to new levels.The University of Cambridges Ce… [+1292 chars]"
+    }]
+}
